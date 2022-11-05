2c72846fb751f4371e1ad9723285db4329568dcd
soruboruit <113847544+soruboruit@users.noreply.github.com>
2022-11-05 21:44:23 +0900

    upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//変数
+let unko = "Hello World!";
+
+//unko = "hello world 2";
+
+//定数
+const bigUnko = "he.. hell... Hello World!"; 
+
+//配列
+const inoki = ["いーち", "にーい", "さーん", "ダーー！！"];
+
+//ループ文
+let index = 0;
+while (index < 5){
+  // 繰り返したい命令を書く
+  console.log(index);
+  index++;
+}
+
